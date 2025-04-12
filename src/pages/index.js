ec475adf128c86b9d1b4825cf004b2c2991c7607
row2k <rowlandrzhang@gmail.com>
2025-04-12 12:39:25 -0400

    fix(doc):fixing descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Learn about the Nerite protocol.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
